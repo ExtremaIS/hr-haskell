c80f5aae535a11e3c93dbe6b51d2907452caa5c0
Travis Cardwell <travis.cardwell@extrema.is>
2021-09-19 10:56:01 +0900

    Update Nix revisions, use GHC 8.10.7

--- test-all.nix
+++ test-all.nix
@@ -9,6 +9,6 @@
   hr-ghc-844 = import ./default.nix { compiler = "ghc844"; };
   hr-ghc-865 = import ./default.nix { compiler = "ghc865"; };
   hr-ghc-884 = import ./default.nix { compiler = "ghc884"; };
-  hr-ghc-8104 = import ./default.nix { compiler = "ghc8104"; };
+  hr-ghc-8107 = import ./default.nix { compiler = "ghc8107"; };
   hr-ghc-901 = import ./default.nix { compiler = "ghc901"; };
 }
