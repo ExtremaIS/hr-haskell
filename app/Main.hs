e25ba329f0e2b64a181b25b73f03b51366ce240b
Travis Cardwell <travis.cardwell@extrema.is>
2023-01-01 09:27:04 +0900

    Update copyright date

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 -- |
 -- Module      : Main
 -- Description : hr: a horizontal rule for terminals
--- Copyright   : Copyright (c) 2019-2022 Travis Cardwell
+-- Copyright   : Copyright (c) 2019-2023 Travis Cardwell
 -- License     : MIT
 --
 -- See the README for details.
